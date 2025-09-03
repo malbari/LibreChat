3f3f21a3762bef1cc6148557f9eb155267c7c00f
Maurizio Albari <maurizio.albari@gmail.com>
2025-09-03 17:42:34 +0200

    fixed

--- synch-with-upstream.sh
+++ synch-with-upstream.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-git checkout main
+#git checkout main
+git remote add upstream https://github.com/danny-avila/LibreChat.git
+git remote -v
 git fetch upstream
 git merge upstream/main --no-edit
-git push origin main
 
